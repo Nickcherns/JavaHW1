87ad3c2851b576c351749c7e81267fd4daf443d5
Nick Chernock <n.chernock@gmail.com>
2023-01-15 17:39:40 -0500

    added content

---
+++ src/experiment.java
@@ -0,0 +1,24 @@
+ 
+public class experiment {
+
+	public static void main(String[] args) {
+		
+		
+		byte number = 3405445; 
+		String incorrect = 45;
+		char wrong = 599694995;
+		
+		String join;
+		boolean tropic;
+		long statement;
+		
+		System.out.print(number);
+		System.out.print(incorrect);
+		System.out.print(wrong);
+		System.out.print(join);
+		System.out.print(tropic);
+		System.out.print(statement);
+		
+	}
+
+}
